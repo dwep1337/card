2111fb1669e360b2c54af501b4cdfa332750dde4
dwep1337 <gemer942@gmail.com>
2025-04-27 21:10:06 -0300

    add(.htaccess, database connection, docker)

---
+++ App/Database/Database.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Database;
+
+use PDO, PDOException;
+
+
+class Database
+{
+    private PDO $connection;
+    private string $host = 'localhost';
+    private string $user = 'root';
+    private string $password = '';
+    private string $database = 'securecard';
+
+    public function __construct()
+    {
+        $this->connect();
+    }
+
+    public function getConnection(): PDO
+    {
+        return $this->connection;
+    }
+
+    private function connect() : void
+    {
+        try {
+            $this->connection = new PDO("mysql:host=$this->host;dbname=$this->database", $this->user, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch (PDOException $e){
+            die("Database connection fail: " . $e->getMessage());
+        }
+    }
+}
